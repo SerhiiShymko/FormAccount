4da4a3bcdfea775a266ce689bbe72d708419cfdf
SerhiiShymko <shimko456@gmail.com>
2023-12-06 22:24:32 +0200

    app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
 import { BookList } from './BookList/BookList';
+import { fetchOrder } from 'api';
 
 export function App() {
+  const [orderItems, setOrderItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //Фетч данних х бекенду
+  useEffect(() => {
+    async function getOrder() {
+      try {
+        setLoading(true);
+        const orderItems = await fetchOrder();
+        setOrderItems(orderItems);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getOrder();
+  }, []);
+
   return (
     <>
       <h1>ЖУРНАЛ ОБЛІКУ НОСІЇВ ІНФОРМАЦІЇ</h1>
-      <BookList />
+      {loading ? <div>LOADING...</div> : <BookList />}
     </>
   );
 }
