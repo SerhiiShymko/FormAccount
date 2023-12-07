aab7f2c4457b218652f2f030918fcd2413ada7eb
SerhiiShymko <shimko456@gmail.com>
2023-12-07 08:38:38 +0200

    fix

---
+++ src/components/SearchBar/SearchBar.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const List = styled.div`
+display: flex;
+flex-direction: row;
+gap: 2px;
+padding-left: 1.5px;
+`;
+
+export const Input = styled.input`
+  width: 130px;
+  text-align:center;
+  `
