df08be9952c6d8084004653835c8eef833c1eb49
SerhiiShymko <shimko456@gmail.com>
2023-12-12 13:42:05 +0200

    error

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -1,9 +1,25 @@
 import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { nanoid } from 'nanoid';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { StyledField, StyledForm } from './BookForm.styled';
+import { StyledError, StyledField, StyledForm } from './BookForm.styled';
 import { useState } from 'react';
 // import { IconButton } from 'components/IconButton/IconButton';
+
+const schema = Yup.object().shape({
+  // selectedDate: Yup.data().required('Required'),
+  serialNumberForm: Yup.string().min(2, 'Too Short!').required('Required'),
+  lastNameOutForm: Yup.string()
+    .oneOf(['Petrov1', 'Petrov2'])
+    .required('Required'),
+  lastNameInForm: Yup.string().min(2, 'Too Short!').required('Required'),
+  aktNumberForm: Yup.number()
+    .positive('Must be >0')
+    .min(1, 'Not enough time!')
+    .required('Required'),
+  noteForm: Yup.string().min(2, 'Too Short!').required('Required'),
+});
 
 export const BookForm = ({ onAdd }) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -12,7 +28,13 @@ export const BookForm = ({ onAdd }) => {
     <Formik
       initialValues={{
         selectedDate: null,
+        serialNumberForm: '',
+        lastNameOutForm: 'Petrov1',
+        lastNameInForm: '',
+        aktNumberForm: '',
+        noteForm: '',
       }}
+      validationSchema={schema}
       onSubmit={values => {
         console.log(values);
       }}
@@ -27,12 +49,22 @@ export const BookForm = ({ onAdd }) => {
         </label>
         <label>
           <StyledField name="serialNumberForm" placeholder="Серійний номер" />
+          <StyledError name="serialNumberForm" component="div" />
         </label>
         <label>
-          <StyledField name="lastNameOutForm" placeholder="ПІБ передав" />
+          <StyledField
+            name="lastNameOutForm"
+            as="select"
+            placeholder="ПІБ передав"
+          >
+            <option value="beginner">Petrov1</option>
+            <option value="intermediate">Petrov2</option>
+          </StyledField>
+          <StyledError name="lastNameOutForm" component="div" />
         </label>
         <label>
           <StyledField name="lastNameInForm" placeholder="ПІБ отримав" />
+          <StyledError name="lastNameInForm" component="div" />
         </label>
         <label>
           <StyledField
@@ -40,9 +72,11 @@ export const BookForm = ({ onAdd }) => {
             placeholder="Акт передачі"
             type="number"
           />
+          <StyledError name="aktNumberForm" component="div" />
         </label>
         <label>
           <StyledField name="noteForm" placeholder="Примітка" />
+          <StyledError name="noteForm" component="div" />
         </label>
         <button type="submit">Add</button>
       </StyledForm>
--- src/components/BookForm/BookForm.styled.js
+++ src/components/BookForm/BookForm.styled.js
@@ -1,24 +1,19 @@
-import { ErrorMessage, Field, Form } from 'formik';
 import styled from 'styled-components';
-
+import { ErrorMessage, Field, Form } from 'formik';
 
 export const StyledField = styled(Field)`
- width: 130px;
- gap: 2px;
- padding-left: 1.5px;
+  width: 130px;
+  gap: 2px;
+  padding-left: 1.5px;
 `;
-
 
 export const StyledForm = styled(Form)`
- display: flex;
- gap: 2px;
+  display: flex;
+  gap: 2px;
   padding-left: 1.5px;
   padding-bottom: 10px;
- 
 `;
 
-export const StylesError = styled(ErrorMessage)`
+export const StyledError = styled(ErrorMessage)`
   color: ${p => p.theme.colors.error};
 `;
-
-
